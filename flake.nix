645a80ec30966aa478b34549aa1ba498e58dd5c5
Yi-Jyun Pan <me@pan93.com>
2026-05-04 13:57:56 +0800

    feat: add skopeo to dev dependencies

--- flake.nix
+++ flake.nix
@@ -60,6 +60,9 @@
               # https://github.com/golangci/golangci-lint
               golangci-lint
 
+              # container tools
+              skopeo
+
               self.formatter.${system}
             ];
           };
